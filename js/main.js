400cb81b383fb932450a635e83746fbb359f2656
Eng-AbdallahEmad <abdallahkhalifa.eng@gmail.com>
2024-01-20 19:54:39 +0200

    Sloving a Problem in the Scroll Reveal

--- js/main.js
+++ js/main.js
@@ -1,36 +1,35 @@
-// احصل على العناصر
-const input = document.querySelector("#password");
-const inputFieldText = document.getElementById("email");
-const inputIcon = document.querySelector(".ShowPassword");
+      // احصل على العناصر
+      const input = document.querySelector("#password");
+      const inputFieldText = document.getElementById("email");
+      const inputIcon = document.querySelector(".ShowPassword");
 
-inputIcon.addEventListener("click", (e) => {
-  e.preventDefault();
+      inputIcon.addEventListener("click", (e) => {
+        e.preventDefault();
 
-  inputIcon.setAttribute(
-    "src",
-    input.getAttribute("type") === "password"
-      ? "imgs/eye-off.svg"
-      : "imgs/eye.svg"
-  );
+        inputIcon.setAttribute(
+          "src",
+          input.getAttribute("type") === "password"
+            ? "imgs/eye-off.svg"
+            : "imgs/eye.svg"
+        );
 
-  input.setAttribute(
-    "type",
-    input.getAttribute("type") === "password" ? "text" : "password"
-  );
-});
+        input.setAttribute(
+          "type",
+          input.getAttribute("type") === "password" ? "text" : "password"
+        );
+      });
 
-// أضف مستمع لحدث التركيز على الـ input
-input.addEventListener("focus", function () {
-  // قم بتغيير رؤية الصورة إلى visible
-  inputIcon.style.visibility = "visible";
-});
+      // أضف مستمع لحدث التركيز على الـ input
+      input.addEventListener("focus", function () {
+        // قم بتغيير رؤية الصورة إلى visible
+        inputIcon.style.visibility = "visible";
+      });
 
-// أضف مستمع لحدث فقدان التركيز عن الـ input
-inputFieldText.addEventListener("focus", function () {
-  // التحقق من وجود نص في عنصر الإدخال
-  if (input.value.trim() === "") {
-    // قم بتغيير رؤية الصورة إلى hidden
-    inputIcon.style.visibility = "hidden";
-  }
-});
-
+      // أضف مستمع لحدث فقدان التركيز عن الـ input
+      inputFieldText.addEventListener("focus", function () {
+        // التحقق من وجود نص في عنصر الإدخال
+        if (input.value.trim() === "") {
+          // قم بتغيير رؤية الصورة إلى hidden
+          inputIcon.style.visibility = "hidden";
+        }
+      });
